94416b340b98b015d082bb7e76bd8a6d399a1e56
Giorgi Bliadze <bliadze1997@gmail.com>
2023-01-26 19:53:00 +0400

    second commit

---
+++ src/components/reducers/UsersReducer.js
@@ -0,0 +1,10 @@
+const UsersReducer = (state = [], action) => {
+  switch (action.type) {
+    case "FETCH_USER":
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+export default UsersReducer;
